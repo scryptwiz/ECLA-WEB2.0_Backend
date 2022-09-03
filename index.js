7991db9bd0903ea1e1927383bbc7f1835808e2e2
Kelvin Ajayi <68742854+Kelvide@users.noreply.github.com>
2022-09-03 20:55:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use('/api', routes)
+app.get('/', (req,res)=>{
+    res.send("Hello World")
+})
 
 app.listen(PORT);
