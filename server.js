948d59bd4e8b0e8b3282b0d6f6316e970bfabee8
Kelvin Ajayi <68742854+Kelvide@users.noreply.github.com>
2022-09-03 20:39:20 +0100

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const app = express();
 const cors = require('cors')
 const routes = require('./Routes/appRoutes')
