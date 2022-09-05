a82dd4e6b5dd5a688b1fb054637c324bef7733ab
Kelvin Ajayi <68742854+Kelvide@users.noreply.github.com>
2022-09-05 08:07:22 +0100

    updated route /user/signup to /user/connect

--- Routes/appRoutes.js
+++ Routes/appRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { editProfile, connect, verifyLogin } = require('../controllers/userController');
 const router = express.Router();
 
-router.post('/user/signup', connect)
+router.post('/user/connect', connect)
 router.post('/user/verify', verifyLogin)
 router.post('/user/editprofile', editProfile)
 
